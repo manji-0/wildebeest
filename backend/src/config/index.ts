8dc24ddcaf4106f6caa6bf0ceed388c6d634b4b2
Wataru Manji <manji@linux.com>
2023-02-11 02:59:37 +0900

    Update index.ts

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -2,7 +2,7 @@ import type { Env } from 'wildebeest/backend/src/types/env'
 import type { JWK } from 'wildebeest/backend/src/webpush/jwk'
 
 export const DEFAULT_THUMBNAIL =
-	'https://imagedelivery.net/NkfPDviynOyTAOI79ar_GQ/b24caf12-5230-48c4-0bf7-2f40063bd400/thumbnail'
+	'https://imagedelivery.net/hPmdm7LZzFimkJSiRHiLSA/d485037d-53fe-46e5-6b31-f3ecb34fc500/avatar'
 
 export function getVAPIDKeys(env: Env): JWK {
 	const value: JWK = JSON.parse(env.VAPID_JWK)
